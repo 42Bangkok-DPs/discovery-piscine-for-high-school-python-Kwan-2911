4f73852051038f935b531de49af9acd1e595d9db
Kwan-2911 <kwanchanok.s@narinukun.ac.th>
2024-10-08 14:43:58 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,9 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+print("a * b = %d" % (a * b))
+if a * b ==0 :
+    print("ZERO")
+elif a * b > 0 :
+    print("POSITIVE")
+else :
+    print("NEGATIVE")
