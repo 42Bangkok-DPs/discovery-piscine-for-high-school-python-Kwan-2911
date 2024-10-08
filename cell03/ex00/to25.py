ef5fe18f78082206679a626eebf5c6fedca61269
Kwan-2911 <kwanchanok.s@narinukun.ac.th>
2024-10-08 15:26:13 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+
+
+number = int(input("enter the number less than 25 :"))
+if number > 25 :
+    print("ERROR")
+while number < 26 :
+    print("Inside the loop, my variable is " + str(number))
+    number += 1
