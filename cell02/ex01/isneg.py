d802f3e438c1536a1116a5f9fe407a4303aae1b1
Kwan-2911 <kwanchanok.s@narinukun.ac.th>
2024-10-08 12:34:48 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = int(input("input your number: "))
+if number < 0:
+    print("This number is negative.")
+elif number == 0:
+    print("This number is both positive ang negative")
+else :
+    print("This number is positive.")
