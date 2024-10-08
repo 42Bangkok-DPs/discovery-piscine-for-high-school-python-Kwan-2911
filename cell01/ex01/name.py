486f1933d082933ed6f63f9ac66e00363971155f
Kwan-2911 <kwanchanok.s@narinukun.ac.th>
2024-10-08 11:15:04 +0700

    Add files via upload

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "KWANCHANOK"
+last_name = "SATTANUN"
+whole_name = first_name + " " + last_name
+print(whole_name)
---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,5 @@
+firstname = input("what's your firstname")
+print(firstname)
+lastname = input("what's your lastname")
+print(lastname)
+print("well,nice to meet you" + firstname + lastname)
