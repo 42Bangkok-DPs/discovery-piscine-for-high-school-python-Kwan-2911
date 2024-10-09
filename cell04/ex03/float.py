9e5975515d4353802a6aaee80f3abe89a3d1af9a
Kwan-2911 <kwanchanok.s@narinukun.ac.th>
2024-10-09 11:20:13 +0700

    Add files via upload

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,6 +1,6 @@
 number = (input("Give me a number : "))
 
 if number == str(float(number)) :
-    print("This number is an integer.")
+    print("This number is a decimal.")
 else :
-    print("This number is a deciaml.")
+    print("This number is an integer.")
